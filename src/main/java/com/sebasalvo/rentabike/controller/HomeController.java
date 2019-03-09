36736993e52e1f7e21db65ffab0de488d43e9c0c
Sebastian Matias Salvo <sebastian.salvo@gmail.com>
2019-03-08 22:42:18 -0300

    - Adding HomeController. - SpringBoot App class. - Cleaning dependencies in pom file.

---
+++ src/main/java/com/sebasalvo/rentabike/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.sebasalvo.rentabike.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Rent-a-Bike dummy home page :)";
+    }
+
+}
